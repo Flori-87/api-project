ce5541cb1b1acebd3331b87f74114c15674fbb93
Flori-87 <fcmelrod@gmail.com>
2020-05-03 23:53:43 +0200

    docker file

--- src/fc_sent_analysis.py
+++ src/fc_sent_analysis.py
@@ -1,7 +1,7 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
-#nltk.download("vader_lexicon")
+nltk.download("vader_lexicon")
 #nltk.download('stopwords')
 
 sia = SentimentIntensityAnalyzer()
